4243221bc06d8e3fe10b019751567d64a6b242dc
sxc1 <56100665+sxc1@users.noreply.github.com>
2025-01-03 01:11:46 -0500

    Added cart size counter and changed 'add to cart' to toggle

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,6 +8,7 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const cartSize = Object.keys(addedToCart).length;
 
     const plantsArray = [
         {
@@ -257,7 +258,9 @@ function ProductList() {
     };
 
     const handleAddToCart = (product) => {
-        dispatch(addItem(product));
+        if (!addedToCart[product.name]) {
+            dispatch(addItem(product));
+        }
         setAddedToCart((prevState) => ({
             ...prevState,
             [product.name]: true,
@@ -281,7 +284,7 @@ function ProductList() {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><span className="cart_quantity_count">{cartSize}</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
             </div>
         </div>
         {!showCart? (
@@ -296,7 +299,12 @@ function ProductList() {
                             <div className="product-title">{plant.name}</div>
                             <div className="product-cost">{plant.cost}</div>
                             <div className="product-description">{plant.description}</div>
-                            <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                            <button
+                                className={`product-button ${addedToCart[plant.name] ? "added-to-cart" : ""}`}
+                                onClick={() => handleAddToCart(plant)}
+                            >
+                                {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
+                            </button>
                         </div>
                         ))}
                     </div>
